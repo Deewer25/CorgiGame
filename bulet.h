008cb9607aa3dccf26115748fb14cec2b2726f3b
Exxplosion <nikitosfly200093@gmail.com>
2020-05-21 03:45:33 +0300

    tryyy

--- bulet.h
+++ bulet.h
@@ -1,8 +1,6 @@
 #ifndef BULET_H
 #define BULET_H
 #include "objects.h"
-#include <SFML/Graphics.hpp>
-#include "hero.h";
 
 //Для правильности работы этого объекта противник, который его использует должен задать начальные скорости и направление.
 class Bulet : public Object
@@ -34,20 +32,17 @@ public:
 
     virtual void draw(sf::RenderWindow &window);
 
-    bool CheckWall(Map& map);
 
     void motion();
 
-    void CheckHero(Hero& hero);
 
     Bulet(const std::string name_file,
          const float obj_size_x,
          const float obj_size_y,
          int pos_x, int pos_y);
 
-    //Bulet(const Bulet& that);
 
-    void update(float time, sf::RenderWindow &window, Map& map);
+    void update(float time, sf::RenderWindow &window);
 };
 
 #endif
