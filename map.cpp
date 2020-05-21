c123b320f679a4d558475ebdb093039d75012d2b
murzagalinasm <sofmur@mail.ru>
2020-05-22 02:24:07 +0300

    добавлены: дополнительные хп, новая карта

--- map.cpp
+++ map.cpp
@@ -16,8 +16,7 @@ void Map::draw(sf::RenderWindow &window){
 	for (int i = 0; i < HEIGHT_MAP; i++)
 		for (int j = 0; j < WIDTH_MAP; j++)
 		{
-            if(TileMap[i][j] ==' ') sprite_map.setTextureRect(IntRect(350,70,70,70));
-			                      if(TileMap[i][j] ==' ') sprite_map.setTextureRect(IntRect(0,0,70,70));
+			if(TileMap[i][j] ==' ') sprite_map.setTextureRect(IntRect(0,0,70,70));
                         if(TileMap[i][j] =='0') sprite_map.setTextureRect(IntRect(0,70,70,70));
                         if(TileMap[i][j] =='1') sprite_map.setTextureRect(IntRect(70,0,70,70));
                         if(TileMap[i][j] =='2') sprite_map.setTextureRect(IntRect(140,0,70,70));
@@ -33,7 +32,10 @@ void Map::draw(sf::RenderWindow &window){
                         if(TileMap[i][j] =='i') sprite_map.setTextureRect(IntRect(280,280,70,70));
                         if(TileMap[i][j] =='*') sprite_map.setTextureRect(IntRect(280,70,70,70));
                         if(TileMap[i][j] =='$') sprite_map.setTextureRect(IntRect(280,0,70,70));
-                        if(TileMap[i][j] =='h') sprite_map.setTextureRect(IntRect(420,0,70,70));
+                        if(TileMap[i][j] =='h') sprite_map.setTextureRect(IntRect(490,490,70,70));
+                        if(TileMap[i][j] =='H') sprite_map.setTextureRect(IntRect(420,0,70,70));
+                        if(TileMap[i][j] =='M') sprite_map.setTextureRect(IntRect(210,210,70,70));
+                        if(TileMap[i][j] =='!') sprite_map.setTextureRect(IntRect(350,210,70,70));
                         if(TileMap[i][j] =='^') sprite_map.setTextureRect(IntRect(350,0,70,70));
                         if(TileMap[i][j] =='W') sprite_map.setTextureRect(IntRect(0,210,70,70));
                         if(TileMap[i][j] =='w') sprite_map.setTextureRect(IntRect(140,210,70,70));
@@ -42,6 +44,10 @@ void Map::draw(sf::RenderWindow &window){
                         if(TileMap[i][j] =='G') sprite_map.setTextureRect(IntRect(490,210,70,70));
                         if(TileMap[i][j] =='S') sprite_map.setTextureRect(IntRect(560,210,70,70));
                         if(TileMap[i][j] =='B') sprite_map.setTextureRect(IntRect(630,210,70,70));
+            		if(TileMap[i][j] =='k') sprite_map.setTextureRect(IntRect(420,280,70,70));
+            		if(TileMap[i][j] =='c') sprite_map.setTextureRect(IntRect(630,280,70,70));
+            		if(TileMap[i][j] =='d') sprite_map.setTextureRect(IntRect(560,420,70,70));
+            		if(TileMap[i][j] =='D') sprite_map.setTextureRect(IntRect(560,490,70,70));
 
 	    		sprite_map.setPosition(j*70,i*70);
 			window.draw(sprite_map);
